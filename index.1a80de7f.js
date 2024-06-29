234be1eb1e71b862058dc9ec5eecd164d317a436
irashovdra <109846925+irashovdra@users.noreply.github.com>
2024-06-29 10:32:10 -0500

    Updates

---
+++ index.1a80de7f.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{let e=document.querySelector(".feedback-form"),t=document.querySelector(".success-message");e.addEventListener("submit",a=>{a.preventDefault(),console.log("Form Data Submitted:",{name:e.name.value,email:e.email.value,message:e.message.value}),t.style.display="block"})});
+//# sourceMappingURL=index.1a80de7f.js.map
